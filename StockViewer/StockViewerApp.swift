1223808193b2d3686fc1cc31034bedd26ca5978a
Jim Long <jalong4@hotmail.com>
2021-02-04 20:50:20 -0800

    Initial Commit

---
+++ StockViewer/StockViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockViewerApp.swift
+//  StockViewer
+//
+//  Created by Jim Long on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct StockViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
